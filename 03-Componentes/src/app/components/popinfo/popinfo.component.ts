78e5421c4ef936fd4ac7d1133d2f9f25d9709dd3
cabrerojas <gcabrerojas@gmail.com>
2019-11-12 14:01:11 -0300

    Ionic Popover

---
+++ 03-Componentes/src/app/components/popinfo/popinfo.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-popinfo',
+  templateUrl: './popinfo.component.html',
+  styleUrls: ['./popinfo.component.scss'],
+})
+export class PopinfoComponent implements OnInit {
+
+  items = Array(40);
+
+  constructor(private popoverCtlr: PopoverController) { }
+
+  ngOnInit() {}
+
+  onClick( valor: number ) {
+
+    console.log(valor);
+
+    this.popoverCtlr.dismiss({
+      item: valor
+    });
+
+  }
+
+
+}
---
+++ 03-Componentes/src/app/pages/popover/popover.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopinfoComponent } from '../../components/popinfo/popinfo.component';
+
+@Component({
+  selector: 'app-popover',
+  templateUrl: './popover.page.html',
+  styleUrls: ['./popover.page.scss'],
+})
+export class PopoverPage implements OnInit {
+
+  constructor( private popoverCtlr: PopoverController) { }
+
+  ngOnInit() {
+  }
+
+  async mostrarPop(ev) {
+      const popover = await this.popoverCtlr.create({
+        component: PopinfoComponent,
+        event: ev,
+        mode: 'ios',
+        backdropDismiss: false
+
+      });
+
+
+      await popover.present();
+
+
+      // const { data } = await popover.onDidDismiss();
+
+      const { data } = await popover.onWillDismiss();
+
+      console.log('Padre' , data);
+
+    }
+
+}
