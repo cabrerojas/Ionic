95bf9bfbc30c9b4426071f0bb7fb8a4e83a5e3d5
cabrerojas <gcabrerojas@gmail.com>
2019-11-14 14:35:03 -0300

    ionic split pane

--- 03-Componentes/src/app/pages/slides/slides.page.ts
+++ 03-Componentes/src/app/pages/slides/slides.page.ts
@@ -8,7 +8,7 @@ import { NavController } from '@ionic/angular';
 })
 export class SlidesPage implements OnInit {
 
-  ocultar='';
+  ocultar = '';
 
 
   slides: { img: string, titulo: string, desc: string }[] = [
