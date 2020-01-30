e5fd42458d96e02cc05ee9200a99297eabf4952e
cabrerojas <gcabrerojas@gmail.com>
2020-01-30 14:27:28 -0300

    FotosGram Server Express

---
+++ 08-FotosGram-Server/classes/token.ts
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken"
+
+export default class Token {
+
+    private static seed: string = 'guillermo-cabrera';
+    private static caducidad: string = '30d';
+
+    constructor() {}
+
+    static getJwtToken ( payload:any ): string{
+        return jwt.sign({
+            usuario: payload
+        }, this.seed, { expiresIn: this.caducidad });
+    }
+
+    static comporbarToken( userToken: string ){
+        
+        return new Promise ((resolve, reject) => {
+
+            jwt.verify(userToken, this.seed, (err, decoded) =>{
+                if (err) {
+                    // no confiar
+                    reject();
+                }
+                else{
+                    //token valido
+                    resolve(decoded);
+                }
+            })
+        });
+        
+    }
+
+
+}
---
+++ 08-FotosGram-Server/index.ts
@@ -0,0 +1,31 @@
+import Server from "./classes/server";
+import userRoutes from "./routes/usuario";
+import mongoose from "mongoose";
+
+import bodyParser from "body-parser"
+
+const server = new Server();
+
+
+// BodyParse
+
+server.app.use( bodyParser.urlencoded({extended:true}) );
+server.app.use( bodyParser.json() );
+
+//Rutas de mi app
+server.app.use( '/user', userRoutes )
+
+//Conectar DB
+mongoose.connect('mongodb://localhost:27017/fotosgram', 
+                { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true, useFindAndModify: false }, (err) => {
+ if (err) throw err;
+ console.log('Base de datos ONLINE');
+ 
+});
+
+
+//Levantar Express
+
+server.start( ()=> {
+    console.log(`Servidor corriendo en puesto: ${server.port}`);
+});
