693272446b339a59d8b8ec76a4be71c12a9040c0
cabrerojas <gcabrerojas@gmail.com>
2020-02-05 16:54:41 -0300

    AhoApp Init

---
+++ AhoApp/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'edit-appointment',
+    loadChildren: () => import('./pages/edit-appointment/edit-appointment.module').then( m => m.EditAppointmentPageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
